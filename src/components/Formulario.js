344ce29e9a22c41d350b12e8b1178a41abaa851d
Miguel Somohano <miguel.spinis@gmail.com>
2020-06-18 23:21:58 -0500

    Creando el state pata citas, y usando evento onChange

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,21 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Formulario = () => {
+
+    // Crear State de citas
+    const [ cita, actualizarCita ] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: '',
+    })
+    // Para poder usar la funcion actualizarCita y llenar el state usamos eventos.
+    // Funcion que se ejecuta cada que el usuario escribe en un input.
+    const actualizarState = () => {
+        console.log('escribiendo...');
+        
+    }
     return ( 
         <Fragment>
             <h1>Crear Cita</h1>
@@ -12,7 +27,8 @@ const Formulario = () => {
                     // Skeleton
                     className="u-full-width"
                     placeholder="Nombre Mascota"
-                    
+                    // Evento en react
+                    onChange={actualizarState}
                 />
                 <label>Nombre Dueño</label>
                 <input
@@ -21,26 +37,33 @@ const Formulario = () => {
                     // Skeleton
                     className="u-full-width"
                     placeholder="Nombre Dueño de la Mascota"
-                    
+                    // Evento en react
+                    onChange={actualizarState}
                 />
                 <label>Fecha</label>
                 <input
                     type="date"
                     name="fecha"
                     // Skeleton
-                    className="u-full-width"                    
+                    className="u-full-width" 
+                    // Evento en react
+                    onChange={actualizarState}                   
                 />
                 <label>Hora</label>
                 <input
                     type="time"
                     name="hora"
                     // Skeleton
-                    className="u-full-width"                    
+                    className="u-full-width" 
+                    // Evento en react
+                    onChange={actualizarState}                   
                 />
                 <label>Síntomas</label>
                 <textarea
                     className="u-full-width"
                     name="sintomas"
+                    // Evento en react
+                    onChange={actualizarState}
                 ></textarea>
                 <button
                     type="submit"
